5e946ea21c3e8a12179ea6f0e1f631105f34bf8c
Rogerio Silva <dgelask8@gmail.com>
2025-09-09 20:23:08 -0300

    Atualiza caminhos de arquivos no HTML e remove arquivos desnecessários, incluindo estilos e scripts relacionados às pizzas.

--- data/pizzas.js
+++ data/pizzas.js
@@ -2,64 +2,64 @@ let pizzaJson = [
     {
         id: 1,
         name: "Mussarela",
-        img: "images/pizza.png",
+        img: "assets/images/pizza.png",
         price: 20.19,
         sizes: ["100g", "530g", "860g"],
         description:
-            "Descrição da pizza em mais de uma linha muito legal bem interessante",
+            "Clássica pizza de mussarela, sabor marcante e textura cremosa irresistível.",
     },
     {
         id: 2,
         name: "Calabresa",
-        img: "images/pizza2.png",
+        img: "assets/images/pizza2.png",
         price: 18.0,
         sizes: ["320g", "530g", "860g"],
         description:
-            "Descrição da pizza em mais de uma linha muito legal bem interessante",
+            "Calabresa fatiada, cebola dourada e temperos especiais, sabor incrível.",
     },
     {
         id: 3,
         name: "Quatro Queijos",
-        img: "images/pizza3.png",
+        img: "assets/images/pizza3.png",
         price: 17.45,
         sizes: ["320g", "530g", "860g"],
         description:
-            "Descrição da pizza em mais de uma linha muito legal bem interessante",
+            "Mistura perfeita de queijos selecionados, cremosa e muito saborosa.",
     },
     {
         id: 4,
         name: "Americana",
-        img: "images/pizza4.png",
+        img: "assets/images/pizza4.png",
         price: 19.77,
         sizes: ["320g", "530g", "860g"],
         description:
-            "Descrição da pizza em mais de uma linha muito legal bem interessante",
+            "Pizza com bacon, ovos e batata palha, combinação deliciosa e única.",
     },
     {
         id: 5,
         name: "Sorvete",
-        img: "images/pizza5.png",
+        img: "assets/images/pizza5.png",
         price: 21.43,
         sizes: ["320g", "530g", "860g"],
         description:
-            "Descrição da pizza em mais de uma linha muito legal bem interessante",
+            "Pizza doce com sorvete cremoso, perfeita para quem ama sobremesa.",
     },
     {
         id: 6,
         name: "Moda da Casa",
-        img: "images/pizza6.png",
+        img: "assets/images/pizza6.png",
         price: 18.55,
         sizes: ["320g", "530g", "860g"],
         description:
-            "Descrição da pizza em mais de uma linha muito legal bem interessante",
+            "Receita exclusiva da casa, ingredientes frescos e sabor surpreendente.",
     },
     {
         id: 7,
         name: "Chocolate",
-        img: "images/pizza7.png",
+        img: "assets/images/pizza7.png",
         price: 22.36,
         sizes: ["320g", "530g", "860g"],
         description:
-            "Descrição da pizza em mais de uma linha muito legal bem interessante",
+            "Cobertura generosa de chocolate, massa macia e sabor irresistível.",
     },
 ];
